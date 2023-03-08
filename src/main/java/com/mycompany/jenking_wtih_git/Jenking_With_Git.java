55178b27900a5e1f0085228afd994d0ce9c5b422
ICX <haribabu81254@gmail.com>
2023-03-08 15:51:26 +0530

    new commit

---
+++ src/main/java/com/mycompany/jenking_wtih_git/Jenking_With_Git.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.jenking_wtih_git;
+
+/**
+ *
+ * @author ICX
+ */
+public class Jenking_With_Git {
+
+    public static void main(String[] args) {
+        
+        
+        int a=10;
+        int b=30;
+        System.out.println("Hello World!    "+a+b);
+    }
+}
